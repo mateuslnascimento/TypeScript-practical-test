4262fb6fcc11077ceec971361ea0bdc372ed3074
Mateus Lopes do Nascimento <mateus-lopes@outlook.com>
2021-03-22 19:33:36 -0300

    Fixing function

--- src/class.ts
+++ src/class.ts
@@ -1,17 +1,27 @@
 'use strict';
 
+type CountResponse = {
+    [key: string]: number;
+}
+
 export default class CharEnumerator {
     char: string;
     
     constructor(char: string) {
         this.char = char;
-    }
+    } 
 
-    countChar() {
+    countChar():CountResponse {
         const arrayOfChar = this.char.split('');
-        
-        const charCounted = arrayOfChar.reduce((total, char) => {
-            return total[char] ? total[char]++ : total[char] = 1;
+    
+        const charCounted = arrayOfChar.reduce((responseObject: CountResponse, char: string) => {
+            
+            const value = responseObject[char] ? responseObject[char]++ : responseObject[char] = 1;
+            
+            responseObject[char] = value;
+
+            return responseObject;
+
         }, {});
 
         return charCounted;
